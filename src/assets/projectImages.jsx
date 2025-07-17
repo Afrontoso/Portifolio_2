7bc2e782a0e78aae5f5e43cb17378ec39289d451
Victor Leandro <83599460+Afrontoso@users.noreply.github.com>
2025-07-17 19:06:55 +0000

    add img fluxograma em react

--- src/assets/projectImages.jsx
+++ src/assets/projectImages.jsx
@@ -3,13 +3,15 @@ import StartSe from './images/StartSe.png';
 import Afrontoso from './images/Afrontoso.png'
 import FluxogramaCurso from './images/Fluxohtml.png'
 import ToDoList from './images/ToDoList.png'
+import FluxoReact from './images/FluxoReact.png'
 
 export const projectImageMap = {
   'OO-CadastroUnB': OO_CadastroUnB,
   'StartSe': StartSe,
   'Afrontoso': Afrontoso,
   'FluxogramaCurso': FluxogramaCurso,
-  'ToDoList': ToDoList
+  'ToDoList': ToDoList,
+  'fluxograma-react': FluxoReact
   
   // Adicione outros projetos aqui
 };
