4cbbe1a6ef7ab759dfb5a709c20bc9685a588636
Victor Leandro <83599460+Afrontoso@users.noreply.github.com>
2025-07-17 18:11:22 +0000

    add img ToDoList

--- src/assets/projectImages.jsx
+++ src/assets/projectImages.jsx
@@ -2,12 +2,14 @@ import OO_CadastroUnB from './images/OO_CadastroUnB.png';
 import StartSe from './images/StartSe.png';
 import Afrontoso from './images/Afrontoso.png'
 import FluxogramaCurso from './images/Fluxohtml.png'
+import ToDoList from './images/ToDoList.png'
 
 export const projectImageMap = {
   'OO-CadastroUnB': OO_CadastroUnB,
   'StartSe': StartSe,
   'Afrontoso': Afrontoso,
-  'FluxogramaCurso': FluxogramaCurso
+  'FluxogramaCurso': FluxogramaCurso,
+  'ToDoList': ToDoList
   
   // Adicione outros projetos aqui
 };
